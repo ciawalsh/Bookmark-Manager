51279fc674603938825ad12f0099dbc4ee079c81
ciawalsh <cia.walsh@gmail.com>
2015-01-13 15:28:10 +0000

    4 tests passing. Added Tags to database

--- server.rb
+++ server.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
 require 'data_mapper'
+
 env = ENV["RACK_ENV"] || "development"
 
 DataMapper.setup(:default, "postgres://localhost:15432/bookmark_manager_#{env}")
 
 require './lib/link' # this needs to be done after datamapper is initialised
+require './lib/tag'
 
 # After declaring your models, you should finalise them
 DataMapper.finalize
@@ -27,6 +29,9 @@ end
 post '/links' do
 	url = params["url"]
 	title = params["title"]
-	Link.create(:url => url, :title => title)
+	tags = params["tags"].split(" ").map do |tag|
+		Tag.first_or_create(:text => tag)
+	end
+	Link.create(:url => url, :title => title, :tags => tags)
 	redirect '/'
 end
